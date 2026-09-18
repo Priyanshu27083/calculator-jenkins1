a300f85697a7a61adcd14db1a32e94256144f2fa
Priyanshu27083 <ugs23159_it.priyanshu@cbit.org.in>
2026-09-18 15:57:59 +0530

    changes

---
+++ src1/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator{
+  public static void main(String[] args){
+    int num1 = 10;
+    int num2 = 5;
+    int res;
+    res = num1 + num2;
+    System.out.println("Result = "+res);
+  }
+}
